17f473459a7182d59191097abe841b3f471727ef
HuiDi Hu <andyhhdi@gmail.com>
2024-06-19 17:49:46 -0700

    temp champion ability showcase

--- frontend/src/components/champion/DemoAndAbilitiesContainer.jsx
+++ frontend/src/components/champion/DemoAndAbilitiesContainer.jsx
@@ -9,37 +9,37 @@ const abilitySlot = ['PASSIVE', '1ST ABILITY', '2ND ABILITY', '3RD ABILITY', 'UL
 
 //TODO: these are placeholders for an import from constants...
 const tempVideoSrc = [
-  'https://www.wildriftfire.com/images/champions/abilities/aatrox-deathbringer-stance.webm',
-  'https://www.wildriftfire.com/images/champions/abilities/aatrox-the-darkin-blade.webm',
-  'https://www.wildriftfire.com/images/champions/abilities/aatrox-infernal-chains.webm',
-  'https://www.wildriftfire.com/images/champions/abilities/aatrox-umbral-dash.webm',
-  'https://www.wildriftfire.com/images/champions/abilities/aatrox-world-ender.webm'
+  'https://assets.contentstack.io/v3/assets/blt370612131b6e0756/blt0503822260ec55b0/63755995a3fffc10b9dc2e83/Aatrox_Passive.mp4',
+  'https://assets.contentstack.io/v3/assets/blt370612131b6e0756/blte131313716699a28/6375599671c75510a0c9f59c/Aatrox_Ability1.mp4',
+  'https://assets.contentstack.io/v3/assets/blt370612131b6e0756/blt009eb30c8a3e3902/63755997fb13fd114111d61a/Aatrox_Ability2.mp4',
+  'https://assets.contentstack.io/v3/assets/blt370612131b6e0756/bltb9e9fc711d7dfcbb/6375598dd4802c10f7f4c166/Aatrox_Ability3.mp4',
+  'https://assets.contentstack.io/v3/assets/blt370612131b6e0756/bltef2efaf871609da2/63755998cca9f010a904bf52/Aatrox_ULT.mp4'
 ]
 const tempChampAbilities = [
   {
-    iconSrc: '../../../assets/champion-icons/EMPTY_CHAMPION.png',
-    name: 'Passive Ability Name',
-    description: 'This is the tempoary passive ability description'
+    iconSrc: 'https://images.contentstack.io/v3/assets/blt370612131b6e0756/bltf02de5aa154436e8/637408773177be104ac204f6/Aatrox_0.jpg',
+    name: 'DEATHBRINGER STANCE',
+    description: "Periodically, Aatrox's next basic attack deals bonus physical damage and heals him, based on the target's max health."
   },
   {
-    iconSrc: '../../../assets/champion-icons/EMPTY_CHAMPION.png',
-    name: '1st Ability Name',
-    description: 'This is the tempoary 1st ability description'
+    iconSrc: 'https://images.contentstack.io/v3/assets/blt370612131b6e0756/blt8a9b72bd71cff770/637408772f1aba10d25a6996/Aatrox_1.jpg',
+    name: 'THE DARKIN BLADE',
+    description: "Aatrox slams his greatsword down, dealing physical damage. He can swing three times, each with a different area of effect."
   },
   {
-    iconSrc: '../../../assets/champion-icons/EMPTY_CHAMPION.png',
-    name: '2nd Ability Name',
-    description: 'This is the tempoary 2nd ability description'
+    iconSrc: 'https://images.contentstack.io/v3/assets/blt370612131b6e0756/blt2647a75d08760657/6374087707d496104f38b139/Aatrox_2.jpg',
+    name: 'INFERNAL CHAINS',
+    description: 'Aatrox smashes the ground, dealing damage to the first enemy hit. Champions and large monsters have to leave the impact area quickly or they will be dragged to the center and take the damage again.'
   },
   {
-    iconSrc: '../../../assets/champion-icons/EMPTY_CHAMPION.png',
-    name: '3rd Ability Name',
-    description: 'This is the tempoary 3rd ability description'
+    iconSrc: 'https://images.contentstack.io/v3/assets/blt370612131b6e0756/blt1dc77fc7124260bb/63740877da728110eb03aeb6/Aatrox_3.jpg',
+    name: 'UMBRAL DASH',
+    description: 'Passively, Aatrox heals when damaging enemy champions. On activation, he dashes in a direction.'
   },
   {
-    iconSrc: '../../../assets/champion-icons/EMPTY_CHAMPION.png',
-    name: 'Ultimate Ability Name',
-    description: 'This is the tempoary ultimate ability description'
+    iconSrc: 'https://images.contentstack.io/v3/assets/blt370612131b6e0756/blt3d3184feb4c01eca/63740877deb37610ecbb373f/Aatrox_4.jpg',
+    name: 'WORLD ENDER',
+    description: 'Aatrox unleashes his demonic form, fearing nearby enemy minions and gaining attack damage, increased healing, and Move Speed. If he gets a takedown, this effect is extended.'
   }
 ]
 
@@ -56,7 +56,7 @@ const DemoAndAbilitiesContainer = () => {
           key={tempVideoSrc[selectedAbility]}
           className='h-[400px] rounded-3xl'
         >
-          <source src={tempVideoSrc[selectedAbility]} type='video/webm' />
+          <source src={tempVideoSrc[selectedAbility]} type='video/mp4' />
           Video doesn't exist :/
         </video>
       </div>
@@ -69,7 +69,7 @@ const DemoAndAbilitiesContainer = () => {
                 onClick={() => { setSelectedAbility(index) }}
               >
                 <img
-                  className='h-20 w-20 rounded-full'
+                  className={`h-20 w-20 rounded-full ${selectedAbility !== index ? 'grayscale-[65%]' : 'grayscale-0'}`}
                   src={ability.iconSrc}
                   alt='/'
                 />
