0eaedb0489d101d2ae09f89c850d4bb3b78605e5
Brandon <167170288+BrandonVH@users.noreply.github.com>
2024-07-14 16:21:16 -0700

    Added comments

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+//Define the function that connects the application to MongoDB. Called in other parts of the application including index.js.
 const connectDB = (url) => {
     return mongoose.connect(url);
 }
