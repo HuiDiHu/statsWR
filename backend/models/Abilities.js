9603aa10da6e9bb5d2f6d36a9f6ba96377e94bdf
Brandon <167170288+BrandonVH@users.noreply.github.com>
2024-07-14 16:50:50 -0700

    Added comments

--- backend/models/Abilities.js
+++ backend/models/Abilities.js
@@ -1,6 +1,8 @@
-const mongoose = require('mongoose');
-const { BadRequestError } = require('../errors');
+const mongoose = require('mongoose'); //required for creating mongoose schemas
+const { BadRequestError } = require('../errors'); //include BadRequestError from errors folder
 
+//Create a basic schema to hold a single ability's video source, icon source, name, and description
+//The values for this schema are located in championAbilities.json
 const AbilitySchema = new mongoose.Schema({
     demoSrc: {
         type: String,
@@ -18,14 +20,15 @@ const AbilitySchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please provide ability description']
     }
-}, {unique: true});
+}, {unique: true}); //{unique: true} ensures that mongoDB will not allow for duplicates of the same object
 
+//Create another schema to link champion names to all their abilities
 const AbilitiesSchema = new mongoose.Schema({
     label: {
         type: String,
         required: [true, 'Please provide champion label']
     },
-    ability: [AbilitySchema]
+    ability: [AbilitySchema] //Holds an array of individual AbilitySchemas. Each array should have 5 indexes. One for the champion's passive, and four for the champion's other abilities
 }, {unique: true});
 
-module.exports = mongoose.model('Abilities', AbilitiesSchema);
+module.exports = mongoose.model('Abilities', AbilitiesSchema); //Export the mongoose model so that 'Abilities' objects can be created using AbilitiesSchema
