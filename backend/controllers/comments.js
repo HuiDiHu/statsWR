0be9cbb8b019cbf8e3928883b168bfd65d8dc0bc
Brandon <167170288+BrandonVH@users.noreply.github.com>
2024-07-15 15:25:08 -0700

    Added comments

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -5,50 +5,59 @@
     - update comment (edit their own comment, upvote/downvote)
     - delete comment (delete comment)
     - send database (commentID + userID + token), retrieve from database to load onto screen, 
-
 */
 
-//require model
-const { StatusCodes } = require('http-status-codes')
-const { BadRequestError, UnauthenticatedError, NotFoundError } = require('../errors')
-const Comment = require('../models/Comment')
-const User = require('../models/User')
-const banList = require('../constants.json')['ban_list']
+const { StatusCodes } = require('http-status-codes') //include status code constants
+const { BadRequestError, UnauthenticatedError, NotFoundError } = require('../errors') //include 3 errors from errors folder
+const Comment = require('../models/Comment') //include Comment schema from Comment.js
+const User = require('../models/User') //include User schema from User.js
+const banList = require('../constants.json')['ban_list'] //include list of all banned users from constants.json
 
-
-const getAllChampionComments = async (req, res) => {
-    //get all comments 
+//get all comments for a specific champion
+const getAllChampionComments = async (req, res) => { 
+    //use destructuring syntax to extract championLabel from the request object
     const { champion: championLabel } = req.query;
+
+    //throw error if championLabel is null
     if (!championLabel) {throw new BadRequestError('Invalid champion page.')}
 
+    //create a comments array with all the comments matching the championLabel from the database
     const comments = await Comment.find({ championLabel })
 
+    //confirm that the function was successful and return the comments array as a json response for frontend display (filter out comments from banned users)
     res.status(StatusCodes.OK).json({ comments: comments.filter((comment) => !banList.includes(comment.user.userID)) })
 }
 
+//handle the process of creating a user comment
 const createComment = async (req, res) => { 
+    //use destructuring syntax to extract ChampionLabel, text, and userID from request object
     const { champion: championLabel } = req.query;
     const { text } = req.body;
     const { userID } = req.user;
+
+    //throw erorrs if any of the variables above are null
     if (!userID) {throw new UnauthenticatedError('Please log in before using this feature.')}
     if (!championLabel) {throw new BadRequestError('Invalid champion page.')}
     const user = await User.findById(userID);
     if (!user) {throw new UnauthenticatedError('Please log in before using this feature.')}
-    
     if (!text) {throw new NotFoundError('Please enter a message.')}
+
+    //create a Comment object in the database with text (the content of the comment), user info (userID, username, and pfp), and championLabel to identify the champion page the comment was made on
     const comment = await Comment.create({ text, user: { userID, username: user.username, profile: user.profile}, championLabel });
 
+    //confirm that the function was successful and return the comment variable as a json response for frontend display
     res.status(StatusCodes.CREATED).json({ comment })
 }
 
+//handle the process of upvoting/downvoting single comment with commentID (unique id)
 const updateComment = async (req, res) => {
+    //use destructuring syntax to extract commentID, upvotes, and downvotes from request object
     const {
         params: { id: commentID }
     } = req;
     const { upvote, downvote } = req.body
-    //update upvotes/downvotes
-    //single comment with commentID (unique id)
-
+    
+    //check if a user has already upvoted or downvoted a comment. ($addToSet is for adding votes, $pull is for retracting votes)
     let updates = {
         "$addToSet": {},
         "$pull": {}
@@ -59,30 +68,39 @@ const updateComment = async (req, res) => {
     if (downvote !== null) {
         downvote ? updates["$addToSet"]["downvotes"] = req.user.userID : updates["$pull"]["downvotes"] = req.user.userID
     }
+
+    //update comment 
     const comment = await Comment.findByIdAndUpdate(commentID, updates, {new: true})
+
+    //confirm that the function was successful and return the comment variable as a json response for frontend display
     res.status(StatusCodes.OK).json({ comment })
 }
 
+//handle the process of deleting a single comment
 const deleteComment = async (req, res) => {
+    //use destructuring syntax to extract commentID from request object
     const {
         params: { id: commentID }
     } = req;
-    //delete single comment
-    //single comment with commentID (unique id)
-
+    
+    //find single comment with commentID (unique id)
     const comment = await Comment.findByIdAndDelete(commentID)
     if (!comment) {
         throw new BadRequestError('Comment not found.')
     }
 
+    //confirm that the function was successful and return the comment variable as a json response for frontend display
     res.status(StatusCodes.OK).json({ comment })
 }
 
+//handle the process of reporting a comment
 const updateReports = async (req, res) => {
+    //use destructuring syntax to extract commentID and text from request object
     const {
         params: { id: commentID }
     } = req;
     const { report } = req.body
+
     let updates = {
         "$addToSet": {},
         "$pull": {}
@@ -92,12 +110,15 @@ const updateReports = async (req, res) => {
     } else {
         updates["$pull"]["reports"] = req.user.userID
     }
+
+    //update comment object in the database to show it has been reported
     await Comment.findByIdAndUpdate(commentID, updates, {new: true})
 
+    //confirm that the function was successful and send a message for frontend display
     res.status(StatusCodes.OK).send('Comment report status successfully updated.')
 }
 
-module.exports = {
+module.exports = { //Exports the functions so they can be used by other parts of the application.
     getAllChampionComments,
     createComment,
     updateComment,
