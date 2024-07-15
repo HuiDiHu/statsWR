29028f46b265cf9123bc4fb1fb0b4c56462ff01d
Brandon <167170288+BrandonVH@users.noreply.github.com>
2024-07-15 15:47:36 -0700

    Added comments

--- backend/populate.js
+++ backend/populate.js
@@ -1,20 +1,20 @@
-require('dotenv').config()
-const connectDB = require('./db/connect')
-const translateRawChampionsData = require('./translateRawChampionsData')
-const Champion = require('./models/Champion')
-const { BadRequestError } = require('./errors')
+require('dotenv').config() //loads environment variables from .env file
+const connectDB = require('./db/connect') //include connectDB function from db folder
+const translateRawChampionsData = require('./translateRawChampionsData') //include translateRawChampionsData function from translateRawChampionsData.js
+const Champion = require('./models/Champion') //include Champion schema defined in Champion.js
+const { BadRequestError } = require('./errors') //include BadRequestError from errors folder
 
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URI)
-        await Champion.deleteMany({})
-        const translatedJson = translateRawChampionsData()
-        await Champion.insertMany(translatedJson)
+        await connectDB(process.env.MONGO_URI) //connect the database using the URI from the env file
+        await Champion.deleteMany({}) //clear all the Champion objects before inserting new ones
+        const translatedJson = translateRawChampionsData() //translate the data from the CURRENT txt files in the rawChampionsData folder
+        await Champion.insertMany(translatedJson) //insert the new Champion objects (translatedJson) into the database
         process.exit()
-    } catch (error) {
+    } catch (error) { //throw error if the statement above fails
         console.log(error)
         throw new BadRequestError('Unable to correctly access MongoDB')
     }
 }
 
-start()
+start() //call function
