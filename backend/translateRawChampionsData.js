0c317e4562f157946103f5f4ca526c6b08945f6a
HuiDi Hu <andyhhdi@gmail.com>
2024-07-19 22:05:36 -0700

    1% winrate = 10% pickrate = 15% banrate

--- backend/translateRawChampionsData.js
+++ backend/translateRawChampionsData.js
@@ -85,9 +85,9 @@ const translateRawChampionsData = () => {
                         winRate: champion.winRate,
                         pickRate: champion.pickRate,
                         banRate: champion.banRate,
-                        weight: (champion.winRate - 50) * 0.75 + champion.pickRate * 0.175 + champion.banRate * 0.075, //0.75 weight for win rate
-                                                                                                                       //0.175 weight for pick rate
-                                                                                                                       //0.075 weight for ban rate
+                        weight: (champion.winRate - 50) + champion.pickRate * 0.1 + champion.banRate * 0.0667, //1 weight for win rate
+                                                                                                              //0.1 weight for pick rate
+                                                                                                              //0.067 weight for ban rate
                         date: new Date(currentDate)
                     }
                 })
