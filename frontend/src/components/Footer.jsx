c2a7269ac7fd83d6093148ab6f01c70a25282117
HuiDi Hu <andyhhdi@gmail.com>
2024-08-22 14:19:37 -0700

    updated footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -22,6 +22,9 @@ const Footer = () => {
                     </Link>
                 </p>
                 <p className='text-xs'>
+                    <span>statsWR isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.</span>
+                </p>
+                <p className='text-xs'>
                     <span>Last updated on 8/18/2024</span>
                 </p>
                 <p className='text-xs line-through'>
