4ce25c3395b4f3e02e3d3f639fe0826721ee47f7
HuiDi Hu <huidihu@utexas.edu>
2024-09-08 17:30:48 -0500

    patch 5.2C

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
                     </Link>
                 </p>
                 <p className='text-xs'>
-                    <span>Last updated on 9/1/2024</span>
+                    <span>Last updated on 9/8/2024</span>
                 </p>
                 <p className='text-xs line-through'>
                     <span>rito pls release official wildrift API</span>
--- frontend/src/constants/index.jsx
+++ frontend/src/constants/index.jsx
@@ -6,7 +6,7 @@ export const navItems = [
 export const regionRankPatch = [
     { label: "\"Global\"", imgSrc: "assets/misc/global-icon.svg" },
     { label: "Diamond+", imgSrc: "assets/misc/diamond-icon.png" },
-    { label: "Version: 5.2B", imgSrc: "/" }
+    { label: "Version: 5.2C", imgSrc: "/" }
 ];
 
 export const allChampions = [
