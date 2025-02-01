585f6ede1c3101995b3f98bb8ed1d21bc4b7dc31
HuiDi Hu <huidihu@utexas.edu>
2025-02-01 16:13:57 -0600

    patch 6.0B

--- frontend/src/constants/index.jsx
+++ frontend/src/constants/index.jsx
@@ -6,7 +6,7 @@ export const navItems = [
 export const regionRankPatch = [
     { label: "\"Global\"", imgSrc: "assets/misc/global-icon.svg" },
     { label: "Diamond+", imgSrc: "assets/misc/diamond-icon.png" },
-    { label: "Version: 6.0A", imgSrc: "/" }
+    { label: "Version: 6.0B", imgSrc: "/" }
 ];
 
 export const allChampions = [
